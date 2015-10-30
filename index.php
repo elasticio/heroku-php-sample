0750bdc98d49c5f946d866a013ef3482747beed7
Tigran Khanzadyan <khanzadyan@users.noreply.github.com>
2015-10-30 14:41:58 +0100

    Removed obsolete links
    Removed links which point to old home page where those pages doesn't exist.

--- index.php
+++ index.php
@@ -11,13 +11,12 @@
 <p>This demo project demonstrate how you can connect your App to Xing,
  LinkedIn, Google Contacts, Salesforce and many other APIs to import
  (and synchronise) contact data.
- See more on <a href="http://www.elastic.io/contact_importer">here</a></p>
+ </p>
  
 <h2>Imported contacts:</h2>
 <ul id="contacts"></ul>
 <script>
 	// Optional identification step
-	// see here for more information http://www.elastic.io/docs#identifying-widget-users
 	elasticio.identify({
 		id : 'my-superuser-id',
 		email : 'chuck@norris.com',
@@ -26,7 +25,6 @@
 	// Registering event listener that will get the data
 	elasticio.on('contact', function(contact) {
 		// Now contact data is stored in 'contact' property
-		// exact format of the contact data see here - http://www.elastic.io/docs#canonical-contact-model
 		var para = document.createElement("li");
 		var node = document.createTextNode(contact.name.firstName + ' ' + contact.name.lastName);
 		para.appendChild(node);
